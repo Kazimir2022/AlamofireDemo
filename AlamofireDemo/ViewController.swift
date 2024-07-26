d297e7ad65e74cb162178c370489d44d655b8bd2
Kazimir Makaranka <m2014ky@gmail.com>
2024-07-26 12:32:29 +0300

    Initial Commit

---
+++ AlamofireDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlamofireDemo
+//
+//  Created by Kazimir on 26.07.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+
+
+}
+
